c584d05d0f2601c7b2f7d1b35831b5f502c6e057
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-06-14 20:03:46 +0300

    Add rest components and routing for entire app

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,24 @@ import Homepage from './pages/homepage';
 import Product from './pages/product';
 import Pricing from './pages/Pricing';
 import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-
-const PageNotFound = () => {
-  return <div>Page Not Found Error 404</div>;
-};
-
-export default PageNotFound;
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Page not found 😢</h1>
+    </div>
+  );
+}
